d74034fcee8077beb30bcf996df80736f31dd74d
individual61 <individual61@users.noreply.github.com>
2018-06-30 09:58:27 -0700

    Got blink and Serial over USB running

--- Code/Testing/btle/R2-DTF-Testing-BTLE/src/main.cpp
+++ Code/Testing/btle/R2-DTF-Testing-BTLE/src/main.cpp
@@ -1,9 +1,19 @@
 #include <Arduino.h>
+#include <SPI.h>
 
-void setup() {
-    // put your setup code here, to run once:
+uint8_t ledPin = 13;
+
+void setup(void)
+{
+  Serial.begin(115200);
+pinMode(ledPin, OUTPUT);
 }
 
-void loop() {
-    // put your main code here, to run repeatedly:
+void loop(void)
+{
+Serial.println("boop");
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(1000);
 }
